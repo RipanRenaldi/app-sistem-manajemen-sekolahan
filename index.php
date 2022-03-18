ac7e5a17c469207bb3ad5a1298b821f43d01bdb8
RipanRenaldi <84433321+RipanRenaldi@users.noreply.github.com>
2022-03-18 13:58:41 +0700

    jadi kapital

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 
 
 		<div class="text">
-			<h1>Sistem Manajemen sekolahan</h1>
+			<h1>Sistem Manajemen Sekolahan</h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
